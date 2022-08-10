ba0678a016e724ab57327097f6ae4cfcf154c87e
Nirobtpi <100894791+Nirobtpi@users.noreply.github.com>
2022-08-10 22:35:09 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+
+let nav = document.querySelector('nav');
+let bars = document.querySelector('.mobile-menu');
+
+
+bars.addEventListener("click", ()=>{
+    // bars.classList.toggle('fa-times');
+    nav.classList.toggle('close')
+  })
+
